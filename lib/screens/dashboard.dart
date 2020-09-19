f3e43dfbd9483b41dd66dcfe5add301cfccb06ce
okayfine <lionel.lobo11@gmail.com>
2020-09-19 19:04:41 +0400

    Update dashboard.dart

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -64,7 +64,7 @@ class _DashboardState extends State<Dashboard> {
             color: Colors.indigo.withOpacity(0.5),
             spreadRadius: 5,
             blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
+            offset: Offset(0, 3),
           ),
         ],
       ),
